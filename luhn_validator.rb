ee6935d2e962dad6b07de08f90a2dc3265a228b6
Supakarn Jariyayong <sup.jariyayong@hotmail.com>
2023-03-08 16:37:04 +0800

    .

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Description: Validates credit card number using Luhn Algorithm
 module LuhnValidator
   # Validates credit card number using Luhn Algorithm
   # arguments: none
@@ -5,7 +8,18 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
-
     # TODO: use the integers in nums_a to validate its last check digit
+    last_digit = nums_a.pop
+    new_nums = nums_a.reverse.map.with_index { |n, i| luhn_multiplier(n, i) }
+    check_digit = (10 - new_nums.sum % 10) % 10
+    check_digit == last_digit
+  end
+
+  def luhn_multiplier(number, index)
+    if index.even?
+      number * 2 > 9 ? (number * 2) - 9 : number * 2
+    else
+      number
+    end
   end
 end
